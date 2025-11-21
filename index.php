ff3e00e450b921244f878f13f78100cb1ddc39db
Fuumi86 <duclaconbo2001@gmail.com>
2025-11-21 15:36:14 +0700

    file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+// Require toàn bộ các file khai báo môi trường, thực thi,...(không require view)
+// Require file Common
+require_once './commons/env.php'; // Khai báo biến môi trường
+require_once './commons/function.php'; // Hàm hỗ trợ
+
+// Require toàn bộ file Controllers
+
+
+// Require toàn bộ file Models
+
+
+// Lấy tham số mode từ URL để phân quyền client/admin
+
+
+?>
