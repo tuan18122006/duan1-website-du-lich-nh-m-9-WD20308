20b70c1a8b728d8d221f1af054af834f5792a46f
quangnd <dinhquang0527@gmail.com>
2025-12-05 16:22:45 +0700

    login update

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -7,35 +7,7 @@
     <title>Travel Login Form</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/style.css">
-    <style>
-        /* Chọn nút đăng nhập trong form login */
-        /* Chọn nút đăng nhập */
-        form.login .field.btn input[type="submit"] {
-            border: 2px solid #007BFF;
-            /* viền xanh */
-            background-color: #007BFF;
-            /* nền xanh */
-            color: white;
-            /* chữ trắng */
-            padding: 10px 20px;
-            /* khoảng cách trong nút */
-            font-size: 16px;
-            border-radius: 5px;
-            /* bo góc */
-            cursor: pointer;
-            /* con trỏ tay khi hover */
-            transition: 0.3s;
-            /* hiệu ứng mượt khi hover */
-        }
-
-        /* Hiệu ứng hover (tăng độ sáng nền) */
-        form.login .field.btn input[type="submit"]:hover {
-            background-color: #0056b3;
-            /* xanh đậm hơn khi hover */
-            border-color: #0056b3;
-            /* viền cùng màu nền khi hover */
-        }
-    </style>
+    
 </head>
 
 <body>
